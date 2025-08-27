0dfec6a108e81c650217d727dca7ad28e48965a8
sunnypogula <sunnypogula28@gmail.com>
2025-08-27 20:02:14 +0300

    initial commit

---
+++ M7 py/cw/LE 3 A4.py
@@ -0,0 +1,7 @@
+import datetime, calendar
+
+current_time= datetime.datetime.today()
+
+print("times nowv at gree.meridian is :",current_time)
+
+print("/n", calendar.calendar(2021))
---
+++ M7 py/cw/LE 3-A3.py
@@ -0,0 +1,11 @@
+num = int(input("enter number  to check:"))
+
+if num>50:
+    print("number is gretert then 50")
+    if num%2==0:
+        print("and it is even too")
+    else:
+        print("and it is old")
+
+else:
+    print("number is less then 50")
