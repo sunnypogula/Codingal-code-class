57fab395716a9c1fad5f1cf8c640e5ecfdf49985
sunnypogula <sunnypogula28@gmail.com>
2025-08-27 19:31:37 +0300

    initial commmiy

---
+++ M7 py/cw/LE 3-A2.py
@@ -0,0 +1,17 @@
+height = float(input("enter your height in cm:"))
+weight = float(input("enter yout weight in kg:"))
+
+BMI = weight / (height/100)**2
+
+print("your BMI is", BMI)
+
+if BMI <= 18.4:
+    print("you are unrweight.") 
+elif BMI <= 24.9:
+    print("you are healthy.") 
+if BMI <= 29.9:
+    print("you are severely over weight.") 
+if BMI <= 34.9:
+    print("you are obese.") 
+if BMI <= 18.4:
+    print("your are severely obese.") 
