2fa306e11606c48c1da8d3032a57fca3d4604477
sunnypogula <sunnypogula28@gmail.com>
2025-08-27 19:15:26 +0300

    initial commit

---
+++ M7 py/cw/LE 3-A1.py
@@ -0,0 +1,8 @@
+number = int(input("enter number to check"))
+print("numberto be checked :", number)
+
+if number%2==0:
+    print("this is an even number")
+
+else:
+    print("this is an odd number")
