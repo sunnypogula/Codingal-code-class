c31a0fdc09c9af194ffc1c2222988c8c66cac18b
sunnypogula <sunnypogula28@gmail.com>
2025-09-15 20:33:05 +0300

    initial commit

---
+++ M8/CS/LE 2.py
@@ -0,0 +1,20 @@
+# Creating a tuple with personal details
+personal_details = (
+    "srujan",         # First Name
+    "pogula",          # Last Name
+    16,             # Age
+    160.5,          # Height in cm
+    50.0,           # Weight in kg
+    "Mathematics"   # Favourite Subject
+)
+
+# Displaying the original tuple
+print("Original Tuple:")
+print(personal_details)
+
+# Converting tuple to list
+personal_details_list = list(personal_details)
+
+# Displaying the converted list
+print("\nConverted List:")
+print(personal_details_list)
