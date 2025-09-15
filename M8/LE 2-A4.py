e63f320194e5ad9c29dd0996f0691c31e9d59bfe
sunnypogula <sunnypogula28@gmail.com>
2025-09-15 20:28:55 +0300

    initial commit

---
+++ M8/LE 2-A4.py
@@ -0,0 +1,10 @@
+setc1 = {"green", "blue"}
+setc2 = {"blue", "yellow"}
+
+print("Original sets:")
+print(setc1)
+print(setc2)
+
+setc = setc1.union(setc2)
+print("\nUnion of above sets:")
+print(setc)
