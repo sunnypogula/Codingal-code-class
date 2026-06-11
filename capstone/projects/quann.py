ca1583f2656dffa4c0bb9b2d899bccbb7f288c32
sunnypogula <sunnypogula28@gmail.com>
2026-06-11 17:03:39 +0300

    intial commit

--- capstone/projects/quann.py
+++ capstone/projects/quann.py
@@ -3,15 +3,15 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv("titanic.csv")
 print(df.head())
-q1 = df["Fare"].quantile(0.25)
-q2 = df["Fare"].quantile(0.50)
-q3 = df["Fare"].quantile(0.75)
+q1 = df["projects"].quantile(0.25)
+q2 = df["projects"].quantile(0.50)
+q3 = df["projects"].quantile(0.75)
 iqr = q3-q1
 print("Q1=",q1)
 print("Median =",q2)
 print("Q3 =",q3)
 print("IQR = ",iqr)
 
-plt.boxplot(df["Fare"])
-plt.title("Boxplot of Passenger Fare")
+plt.boxplot(df["projects"])
+plt.title("Boxplot of Passenger projects")
 plt.show()
